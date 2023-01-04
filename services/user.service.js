f1af7cd6be2b4c7b384439227574937ea48a21f1
아헨의 MacBook <dkgps@icloud.com>
2023-01-05 00:12:49 +0900

    카카오 로그인 및 회원가입 API

--- services/user.service.js
+++ services/user.service.js
@@ -1,4 +1,50 @@
 const { user } = require('../models/index'); // ./models/index.js에서 설정한 연결된 모델들을 가져온다
+const axios = require('axios');
+const kakaoLogin = async (req, res, next) => {
+	try
+	{
+		const { accessToken } = req;
+		// 카카오 회원 정보 가져오기
+		let kakaoInfo = await axios({
+            method:'get',
+            url:'https://kapi.kakao.com/v2/user/me',
+            headers:{
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+
+		let userDto = {};
+		userDto.userEmail = kakaoInfo.data.kakao_account.email; // 이메일
+		userDto.socialKey = kakaoInfo.data.id;                  // 소셜키
+		userDto.social	  = 'kakao'								// 소셜 : 카카오
+
+		// 회원 존재 여부 확인
+		const kakaoUser = await user.findOne({
+			where: { 
+				social :  userDto.social,
+				socialKey : userDto.socialKey
+			}
+		});
+		
+		// 회원이 있으면 로그인
+		if(kakaoUser)
+		{
+			return { signUp : false }
+		}
+		// 없으면 회원 가입
+		{
+			await user.create(userDto);
+			return { signUp : true }
+		}
+		
+	}
+	catch (err)
+	{
+		console.error(err);
+        throw Error(`ERROR WHILE KAKAO LOGIN - ${err}`);
+	}
+	
+}
 
 // 전체 회원 리스트
 const getAllUsers = async (req, res, next) => {
@@ -63,6 +109,7 @@ const deleteUser = async (req, res, next) => {
 }
 
 module.exports = {
+	kakaoLogin,
     getAllUsers,
     getUser,
     insertUser,
