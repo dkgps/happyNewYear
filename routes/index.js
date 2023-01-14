5f3dbcbe73cd952b68437882c310871490cd7984
아헨의 MacBook <dkgps@icloud.com>
2023-01-14 10:59:09 +0900

    prefix /auth 추가, 에러 페이지 출력, callback url 변경

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,9 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 
-router.get('/', function(req,res,next) {
-	res.render('index',{
-		BASE_URL			 : process.env.BASE_URL,
-		KAKAO_JAVASCRIPT_KEY : process.env.KAKAO_JAVASCRIPT_KEY,
-	})
-})
-
-router.get('/kakaoCallback', function(req,res,next) {
-	res.render('kakaoCallback',{
-		BASE_URL		   : process.env.BASE_URL,
-		KAKAO_REST_API_KEY : process.env.KAKAO_REST_API_KEY,
-	})
-})
+// 에러 페이지 출력
+router.use((req, res, next) => {
+    res.status(400).render('error');
+});
 
 module.exports = router;
